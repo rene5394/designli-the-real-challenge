9ac60d31b2b48ad64b8a260566f00e53ab56e481
Rene Torres <rene_edgardo_2@hotmail.com>
2024-09-22 11:00:08 -0600

    Create functionality that returns the JSON file as link the email's body

--- src/email-parser/email-parser.service.ts
+++ src/email-parser/email-parser.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { join } from 'path';
 import { simpleParser } from 'mailparser';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class EmailParserService {
@@ -10,10 +11,33 @@ export class EmailParserService {
     const file = createReadStream(join(process.cwd(), ...splitFilePath));
     const parsedEmail = await simpleParser(file);
     const attachments = parsedEmail.attachments;
-    const jsonBuffer = attachments
-      .filter((attachment) => attachment.contentType === 'application/json')
-      .map((attachment) => attachment.content);
+    const html = parsedEmail.html;
+    const links: string[] = [];
 
-    return JSON.parse(jsonBuffer[0].toString('utf8'));
+    if (attachments.length > 0) {
+      const jsonBuffer = attachments
+        .filter((attachment) => attachment.contentType === 'application/json')
+        .map((attachment) => attachment.content);
+
+      return JSON.parse(jsonBuffer[0].toString('utf8'));
+    }
+
+    if (html) {
+      const $ = cheerio.load(html);
+
+      $('a').each((_, element) => {
+        const href = $(element).attr('href');
+        if (href) {
+          links.push(href);
+        }
+      });
+
+      const urlJson = links.find((link) => link.includes('.json'));
+      if (urlJson) {
+        const response = await fetch(urlJson);
+        const data = await response.json();
+        return data;
+      }
+    }
   }
 }
