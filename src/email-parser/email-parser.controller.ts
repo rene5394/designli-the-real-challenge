d9c8d88d226ea86620c377a0ea277df850ad8504
Rene Torres <rene_edgardo_2@hotmail.com>
2024-09-22 18:37:25 -0600

    Refactor setting properlies status code on parseEmail function

--- src/email-parser/email-parser.controller.ts
+++ src/email-parser/email-parser.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  HttpCode,
+} from '@nestjs/common';
 import { EmailParserService } from './email-parser.service';
 
 @Controller('email-parser')
@@ -6,7 +13,15 @@ export class EmailParserController {
   constructor(private readonly emailParserService: EmailParserService) {}
 
   @Post()
-  parseEmail(@Body('path') emailFilePath: string) {
+  @HttpCode(HttpStatus.OK)
+  parseEmail(@Body('path') emailFilePath: string): Promise<JSON> {
+    if (!emailFilePath) {
+      throw new HttpException(
+        'emailFilePath is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     return this.emailParserService.parseEmail(emailFilePath);
   }
 }
