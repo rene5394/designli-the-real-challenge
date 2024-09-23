776b24a494b349b6340314889e744cd87262147f
Rene Torres <rene_edgardo_2@hotmail.com>
2024-09-22 18:38:47 -0600

    Refactor adding try catch and setting properlies status code on parseEmail function

--- src/email-parser/email-parser.service.ts
+++ src/email-parser/email-parser.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { createReadStream } from 'fs';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { createReadStream, existsSync } from 'fs';
 import { join } from 'path';
 import { simpleParser } from 'mailparser';
 import {
@@ -9,19 +9,35 @@ import {
 
 @Injectable()
 export class EmailParserService {
-  async parseEmail(emailFilePath: string) {
-    const splitFilePath = emailFilePath.split('/');
-    const file = createReadStream(join(process.cwd(), ...splitFilePath));
-    const parsedEmail = await simpleParser(file);
-    const attachments = parsedEmail.attachments;
-    const html = parsedEmail.html;
+  async parseEmail(emailFilePath: string): Promise<JSON> {
+    try {
+      const splitFilePath = emailFilePath.split('/');
+      const filePath = join(process.cwd(), ...splitFilePath);
 
-    if (attachments.length > 0) {
-      return jsonAttachmentParser(attachments);
-    }
+      if (!existsSync(filePath)) {
+        throw new HttpException('File not found', HttpStatus.NOT_FOUND);
+      }
 
-    if (html) {
-      return await jsonLinkFileParser(html);
+      const file = createReadStream(filePath);
+      const parsedEmail = await simpleParser(file);
+      const attachments = parsedEmail.attachments;
+      const html = parsedEmail.html;
+
+      if (attachments.length > 0) {
+        return jsonAttachmentParser(attachments);
+      }
+
+      if (html) {
+        return await jsonLinkFileParser(html);
+      }
+    } catch (error) {
+      if (error.status === HttpStatus.NOT_FOUND) {
+        throw new HttpException('File not found', HttpStatus.NOT_FOUND);
+      }
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
